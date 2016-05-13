74bed75a451083b9fc93489869099fdad4e87fc2
DB Wilson jr <dbwilsonjr@gmail.com>
2016-05-13 14:15:19 -0400

    Initial

---
+++ DR-Market-Jade-Value/Form1.Designer.cs
@@ -0,0 +1,316 @@
+﻿namespace DR_Market_Jade_Value
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblGJAvg = new System.Windows.Forms.Label();
+            this.lblGJLow = new System.Windows.Forms.Label();
+            this.lblGJHigh = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.lblGJade = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cmbJLevel = new System.Windows.Forms.ComboBox();
+            this.lblJPrice = new System.Windows.Forms.Label();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label9 = new System.Windows.Forms.Label();
+            this.lblHighRat = new System.Windows.Forms.Label();
+            this.lblLowRat = new System.Windows.Forms.Label();
+            this.lblAvgRat = new System.Windows.Forms.Label();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.lblGJAvg);
+            this.groupBox1.Controls.Add(this.lblGJLow);
+            this.groupBox1.Controls.Add(this.lblGJHigh);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.lblGJade);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.cmbJLevel);
+            this.groupBox1.Controls.Add(this.lblJPrice);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(267, 155);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Market Jade Options";
+            // 
+            // lblGJAvg
+            // 
+            this.lblGJAvg.AutoSize = true;
+            this.lblGJAvg.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGJAvg.Location = new System.Drawing.Point(157, 130);
+            this.lblGJAvg.Name = "lblGJAvg";
+            this.lblGJAvg.Size = new System.Drawing.Size(41, 15);
+            this.lblGJAvg.TabIndex = 7;
+            this.lblGJAvg.Text = "label9";
+            // 
+            // lblGJLow
+            // 
+            this.lblGJLow.AutoSize = true;
+            this.lblGJLow.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGJLow.Location = new System.Drawing.Point(157, 103);
+            this.lblGJLow.Name = "lblGJLow";
+            this.lblGJLow.Size = new System.Drawing.Size(41, 15);
+            this.lblGJLow.TabIndex = 6;
+            this.lblGJLow.Text = "label8";
+            // 
+            // lblGJHigh
+            // 
+            this.lblGJHigh.AutoSize = true;
+            this.lblGJHigh.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGJHigh.Location = new System.Drawing.Point(157, 76);
+            this.lblGJHigh.Name = "lblGJHigh";
+            this.lblGJHigh.Size = new System.Drawing.Size(41, 15);
+            this.lblGJHigh.TabIndex = 5;
+            this.lblGJHigh.Text = "label7";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(6, 130);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(88, 15);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Average Value:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(6, 103);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(83, 15);
+            this.label5.TabIndex = 1;
+            this.label5.Text = "Lowest Value:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(6, 76);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(86, 15);
+            this.label4.TabIndex = 1;
+            this.label4.Text = "Highest Value:";
+            // 
+            // lblGJade
+            // 
+            this.lblGJade.AutoSize = true;
+            this.lblGJade.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGJade.Location = new System.Drawing.Point(157, 49);
+            this.lblGJade.Name = "lblGJade";
+            this.lblGJade.Size = new System.Drawing.Size(41, 15);
+            this.lblGJade.TabIndex = 3;
+            this.lblGJade.Text = "label4";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(6, 49);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(129, 15);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Guild Jade Equivalent:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(121, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(68, 15);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Jade Price:";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(6, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(69, 15);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Jade Level:";
+            // 
+            // cmbJLevel
+            // 
+            this.cmbJLevel.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbJLevel.FormattingEnabled = true;
+            this.cmbJLevel.Location = new System.Drawing.Point(81, 19);
+            this.cmbJLevel.Name = "cmbJLevel";
+            this.cmbJLevel.Size = new System.Drawing.Size(32, 23);
+            this.cmbJLevel.TabIndex = 1;
+            this.cmbJLevel.SelectedIndexChanged += new System.EventHandler(this.cmbJLevel_SelectedIndexChanged);
+            // 
+            // lblJPrice
+            // 
+            this.lblJPrice.AutoSize = true;
+            this.lblJPrice.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblJPrice.Location = new System.Drawing.Point(195, 22);
+            this.lblJPrice.Name = "lblJPrice";
+            this.lblJPrice.Size = new System.Drawing.Size(41, 15);
+            this.lblJPrice.TabIndex = 0;
+            this.lblJPrice.Text = "label1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.lblAvgRat);
+            this.groupBox2.Controls.Add(this.lblLowRat);
+            this.groupBox2.Controls.Add(this.lblHighRat);
+            this.groupBox2.Controls.Add(this.label9);
+            this.groupBox2.Controls.Add(this.label8);
+            this.groupBox2.Controls.Add(this.label7);
+            this.groupBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox2.Location = new System.Drawing.Point(12, 173);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(267, 100);
+            this.groupBox2.TabIndex = 1;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Gem-to-Guild Coin Ratios";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(9, 20);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(115, 15);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "High GC Cost Ratio:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(9, 47);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(112, 15);
+            this.label8.TabIndex = 1;
+            this.label8.Text = "Low GC Cost Ratio:";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label9.Location = new System.Drawing.Point(9, 74);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(133, 15);
+            this.label9.TabIndex = 2;
+            this.label9.Text = "Average GC Cost Ratio:";
+            // 
+            // lblHighRat
+            // 
+            this.lblHighRat.AutoSize = true;
+            this.lblHighRat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHighRat.Location = new System.Drawing.Point(157, 18);
+            this.lblHighRat.Name = "lblHighRat";
+            this.lblHighRat.Size = new System.Drawing.Size(48, 15);
+            this.lblHighRat.TabIndex = 3;
+            this.lblHighRat.Text = "label10";
+            // 
+            // lblLowRat
+            // 
+            this.lblLowRat.AutoSize = true;
+            this.lblLowRat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLowRat.Location = new System.Drawing.Point(157, 47);
+            this.lblLowRat.Name = "lblLowRat";
+            this.lblLowRat.Size = new System.Drawing.Size(48, 15);
+            this.lblLowRat.TabIndex = 4;
+            this.lblLowRat.Text = "label11";
+            // 
+            // lblAvgRat
+            // 
+            this.lblAvgRat.AutoSize = true;
+            this.lblAvgRat.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.5F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAvgRat.Location = new System.Drawing.Point(157, 74);
+            this.lblAvgRat.Name = "lblAvgRat";
+            this.lblAvgRat.Size = new System.Drawing.Size(48, 15);
+            this.lblAvgRat.TabIndex = 5;
+            this.lblAvgRat.Text = "label12";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(286, 281);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jade Value";
+            this.Load += new System.EventHandler(this.frmLoad);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label lblGJAvg;
+        private System.Windows.Forms.Label lblGJLow;
+        private System.Windows.Forms.Label lblGJHigh;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label lblGJade;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cmbJLevel;
+        private System.Windows.Forms.Label lblJPrice;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label lblAvgRat;
+        private System.Windows.Forms.Label lblLowRat;
+        private System.Windows.Forms.Label lblHighRat;
+    }
+}
+
---
+++ DR-Market-Jade-Value/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DR_Market_Jade_Value
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void frmLoad(object sender, EventArgs e)
+        {
+            cmbJLevel.Items.Add(4);
+            cmbJLevel.Items.Add(5);
+            cmbJLevel.Items.Add(6);
+            lblJPrice.Text = "";
+            lblGJade.Text = "";
+            lblGJHigh.Text = "";
+            lblGJLow.Text = "";
+            lblGJAvg.Text = "";
+            lblHighRat.Text = "";
+            lblLowRat.Text = "";
+            lblAvgRat.Text = "";
+        }
+
+        /// <summary>
+        /// This is basically the program. Upon choosing one of the combobox options, the program figures out how many guild coins are saved by spending gems on the
+        /// level of jade selected and below, finds how much value you're getting per Gem by spending gems instead of GCs. The premise of the program is that Gems
+        /// are more valuable and that a certain value must be met before it's worth spending gems on lv4+ jades instead of just waiting for daily/weekly GCs.
+        /// The price of each Jade differs per type, so we do the math for the highest jade (True Damage at 100GC), the lowest Jade (Armor at 30GC), and the average of all 
+        /// Jades (53.7, rounded to 54).
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void cmbJLevel_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            int jadeLvl = 0;
+            int JH = 0, JL = 0, JA = 0;
+            double HRat = 0, LRat = 0, ARat = 0;
+
+            int.TryParse(cmbJLevel.SelectedItem.ToString(), out jadeLvl);
+
+            //Determine base price of market jade.
+            switch (jadeLvl)
+            {
+                case 4: lblJPrice.Text = "68 Gems"; break;
+                case 5: lblJPrice.Text = "165 Gems"; break;
+                case 6: lblJPrice.Text = "396 Gems"; break;
+                default: lblJPrice.Text = "Read Error"; break;
+            }
+
+            //Get math for guild costs of equal leveled jades
+            JadeMath(jadeLvl - 3, out JH, out JL, out JA);
+
+            //Output guild costs
+            lblGJade.Text = (Math.Pow(3,(jadeLvl - 3))).ToString() + " lvl 3 Jades";
+            lblGJHigh.Text = JH.ToString() + " guild coins";
+            lblGJLow.Text = JL.ToString() + " guild coins";
+            lblGJAvg.Text = JA.ToString() + " guild coins";
+
+            //Do Gem:GC Ratio Math
+            GemMath(jadeLvl, JH, JL, JA, out HRat, out LRat, out ARat);
+
+            //Output Ratios
+            lblHighRat.Text = HRat.ToString("N2") + " GCs per Gem";
+            lblLowRat.Text = LRat.ToString("N2") + " GCs per Gem";
+            lblAvgRat.Text = ARat.ToString("N2") + " GCs per Gem";
+        }
+
+        /// <summary>
+        /// This function determines how many guild coins are required to get the same jade level you can buy from the market.
+        /// </summary>
+        /// <param name="JLvl"></param>
+        /// <param name="Jhigh"></param>
+        /// <param name="JLow"></param>
+        /// <param name="JAvg"></param>
+        public void JadeMath(int JLvl, out int Jhigh, out int JLow, out int JAvg)
+        {
+            Jhigh = 0;
+            JLow = 0;
+            JAvg = 0;
+            int guildJadeNum = (int)Math.Pow(3,JLvl);
+            Jhigh = guildJadeNum * 100;
+            JLow = guildJadeNum * 30;
+            JAvg = guildJadeNum * 54;
+        }
+
+        /// <summary>
+        /// This function find the ratios for guild coins per gem. It's basic af.
+        /// </summary>
+        /// <param name="JLv"></param>
+        /// <param name="JHigh"></param>
+        /// <param name="JLow"></param>
+        /// <param name="JAvg"></param>
+        /// <param name="HighRate"></param>
+        /// <param name="LowRate"></param>
+        /// <param name="AvgRate"></param>
+        public void GemMath(int JLv, int JHigh, int JLow, int JAvg, out double HighRate, out double LowRate, out double AvgRate)
+        {
+            HighRate = 0;
+            LowRate = 0;
+            AvgRate = 0;
+            double jadeVal = 0;
+
+            switch (JLv)
+            {
+                case 4: jadeVal = 68; break;
+                case 5: jadeVal = 165; break;
+                case 6: jadeVal = 396; break;
+                default: MessageBox.Show("Something went pretty wrong here. You should close this thing out", "Something borked!", MessageBoxButtons.OK, MessageBoxIcon.Exclamation); break;
+            }
+
+            HighRate = (JHigh / jadeVal);
+            LowRate = (JLow / jadeVal);
+            AvgRate = (JAvg / jadeVal);
+        }
+
+    }
+}
